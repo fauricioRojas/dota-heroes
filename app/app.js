5baf663a8e7e7321678aad786aa10cc2937a45a7
Fauricio Rojas <fauricio.rojas@hpe.com>
2016-10-09 16:12:03 -0600

    Clean branch master

--- app/app.js
+++
@@ -1,9 +0,0 @@
-/*jshint esversion: 6 */
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-const app = document.getElementById('app');
-ReactDOM.render(
-  <h1>Hello world!!</h1>, app
-);
