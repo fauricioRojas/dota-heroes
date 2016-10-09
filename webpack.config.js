f98a405cc9c200a20621dcea682bacb788fa0491
Fauricio Rojas <fauricio.rojas@hpe.com>
2016-10-09 16:06:08 -0600

    Project configuration

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var paths = {
+  entryUrl: './app/app.js',
+  outputUrl: './temp/js',
+  indexUrl: './app/index.html',
+  indexUrlDest: '../index.html',
+  assetsUrl: './app/assets',
+  assetsUrlDest: '../assets',
+};
+
+module.exports = {
+  devServer: {
+    inline: true,
+    contentBase: 'temp',
+    port: 3000
+  },
+  entry: paths.entryUrl,
+  output: {
+    path: paths.outputUrl,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.css', '.scss', '.sass']
+  },
+  context: __dirname,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$|\.scss$/,
+        loader: 'style!css!scss!sass',
+      }
+    ]
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['temp'], {
+    //   root: './',
+    //   verbose: true,
+    //   dry: false,
+    //   exclude: []
+    // }),
+    new CopyWebpackPlugin([
+      {
+        from: paths.indexUrl,
+        to: paths.indexUrlDest
+      },
+      {
+        context: paths.assetsUrl,
+        from: '**/*',
+        to: paths.assetsUrlDest
+      }
+    ])
+  ]
+};
