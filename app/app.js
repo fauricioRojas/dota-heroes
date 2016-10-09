19ecf7501efb95fd47043766c078e06c50ab818b
Fauricio Rojas <fauricio.rojas@hpe.com>
2016-10-09 15:35:22 -0600

    Project configuration

---
+++ app/app.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const app = document.getElementById('app');
+ReactDOM.render(
+  <h1>Hello world!!</h1>, app
+);
